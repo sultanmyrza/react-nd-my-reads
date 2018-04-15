1b93307a00ff88d0a27063aa8209cf8bf72f8385
sultanmyrza <sultanmyrza@gmail.com>
2018-04-15 11:11:22 +0900

    refactor fetch books from remote endpoint

--- src/App.js
+++ src/App.js
@@ -4,16 +4,24 @@ import {Route} from 'react-router-dom';
 import './App.css'
 import SearchBooks from './Components/SearchBooks';
 import ListBooks from './Components/ListBooks';
+import * as BooksApi from './Services/BooksApi';
 
 class BooksApp extends React.Component {
   state = {
+    books: []
+  }
+
+  componentDidMount() {
+    BooksApi.getAll().then(data => {
+      this.setState({ books: data })
+    })
   }
 
   render() {
     return (
       <div className="app">
         <Route exact path="/" render={() => (
-          <ListBooks />
+          <ListBooks books={this.state.books}/>
         )} />
 
         <Route path="/search" render={() => (
